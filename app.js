07e41f47f5facaa4ab32c9a01077b7e2c6944c2e
Uihyun Hong <mac@Uihyunui-MacBookAir.local>
2023-07-10 17:03:46 +0900

    cors fix

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
-const allowedOrigins = [ "http://localhost:3001","http://localhost:3000" , "ec2-43-201-220-178.ap-northeast-2.compute.amazonaws.com"]
+const allowedOrigins = [ "http://localhost:3001","http://localhost:3000" , "http://ec2-43-201-220-178.ap-northeast-2.compute.amazonaws.com"]
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
